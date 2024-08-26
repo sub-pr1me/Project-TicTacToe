d3b911314e81feb6b79161ae1e92ea9c55697fe7
sub-prime <>
2024-08-26 22:00:22 +0300

    Rebuild the main functionality script

--- script.js
+++ script.js
@@ -1,52 +1,82 @@
-const game = (function () {
-    let board = ['','','','','','','','','',];
-    const playerX = {score: 0, moves: 0};
-    const playerO = {score: 0, moves: 0};
+const obj = (function () {
+    const body = document.querySelector('body');
+    const display = document.querySelector('.display');
+    const container = document.querySelector('.container');
+    const fields = document.querySelector('.field');
+    const btn = document.querySelector('button');
+    let board = ['', '', '', '', '', '', '', '', ''];
+    const playerX = {moves: 0};
+    const playerO = {moves: 0};
+    return { body, display, container, fields, btn, board, playerX, playerO};
+})();
 
-    const whichTurn = () => {
-        return (playerX.moves > playerO.moves) ? 'O' : 'X'; 
+const func = (function () {
+
+    const greeting = () => {
+        obj.display.textContent = 'MAKE YOUR MOVE!'
     };
-    const startGame = () => {
-        const fields = document.querySelectorAll('.field');
-            fields.forEach((field) => {
-            field.addEventListener('click', () => {
-                let num = field.className.slice(-1);
-                board[num-1] = whichTurn();
-                field.textContent = board[num-1];
-                if (field.textContent === 'X') {
-                    playerX.moves++
+
+    const whosTurn = () => {
+        return (obj.playerX.moves > obj.playerO.moves) ? 'O' : 'X'; 
+    };
+
+    const playerMove = () => {
+        obj.container.addEventListener('click', (e) => {
+            if (e.target.classList.contains('field')
+                && e.target.textContent === ''
+                && obj.display.textContent !== 'PLAYER 1 WINS!'
+                && obj.display.textContent !== 'PLAYER 2 WINS!'
+                && obj.display.textContent !== `IT'S A TIE!`) {
+                let num = e.target.className.slice(-1);
+                obj.board[num-1] = whosTurn();
+                e.target.textContent = whosTurn();
+
+                if (e.target.textContent === 'X') {
+                    obj.playerX.moves++
                 } else {
-                    playerO.moves++
+                    obj.playerO.moves++
                 };
-                gameStatus();
-            }, { once: true });
+                // console.log(obj.board);
+                obj.display.textContent = displayContent(gameStatus());
+            };            
         });
     };
+
+    const displayContent = (status) => {
+        return status ? status : 'GAME IN PROGRESS';
+    };
+
     const gameStatus = () => {
-        const win = [
-            [board[0],board[1],board[2]],
-            [board[3],board[4],board[5]],
-            [board[6],board[7],board[8]],
-            [board[0],board[3],board[6]],
-            [board[1],board[4],board[7]],
-            [board[2],board[5],board[8]],
-            [board[0],board[4],board[8]],
-            [board[2],board[4],board[6]]];
-        const display = document.querySelector('.display');
-        for (item of win) {
-            if (!item.includes('O') && !item.includes('')) {
-                display.textContent = 'PLAYER 1 WINS!';
-                break;                
-            } else if (!item.includes('X') && !item.includes('')) {
-                display.textContent = 'PLAYER 2 WINS!';
-                break;                
-            } else if (item.includes('X') && item.includes('O') && !board.includes('')) {
-                display.textContent = `IT'S A TIE!`;            
-            } else {
-                display.textContent = 'GAME IN PROGRESS';
+        let winCombos = [
+        [obj.board[0], obj.board[1], obj.board[2]],
+        [obj.board[3], obj.board[4], obj.board[5]],
+        [obj.board[6], obj.board[7], obj.board[8]],
+        [obj.board[0], obj.board[3], obj.board[6]],
+        [obj.board[1], obj.board[4], obj.board[7]],
+        [obj.board[2], obj.board[5], obj.board[8]],
+        [obj.board[0], obj.board[4], obj.board[8]],
+        [obj.board[2], obj.board[4], obj.board[6]]];        
+        for (let i=0; i<winCombos.length; i++) {
+            if (!winCombos[i].includes('O')
+                && !winCombos[i].includes('')) {
+                return 'PLAYER 1 WINS!';
+
+            } else if (!winCombos[i].includes('X')
+                && !winCombos[i].includes('')) {
+                return 'PLAYER 2 WINS!';
+
+            } else if (!obj.board.includes('') && i===winCombos.length) {
+                return `IT'S A TIE!`;
+
+            } else if (!obj.board.includes('X') 
+                && !obj.board.includes('O')) {
+                return 'MAKE YOUR MOVE!';
             };
         };
     };
-    return {board, playerX, playerO, whichTurn, startGame, gameStatus};
+    return { greeting, whosTurn, playerMove, gameStatus, displayContent,};
 })();
-game.startGame();
+
+func.greeting();
+func.whosTurn();
+func.playerMove();
